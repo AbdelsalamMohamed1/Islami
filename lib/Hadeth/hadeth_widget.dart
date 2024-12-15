38cb23b1d0b02947bcdf6f33c9522dffe8988e7a
Abdulsalam mohamed <abdulsalam0mhamed@gmail.com>
2024-12-15 22:07:34 +0200

    Finish Quran,Hadeth,Sebha and there problem in radio

---
+++ lib/Hadeth/hadeth_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../data_features/my_theme_data.dart';
+
+class HadethDisplay extends StatelessWidget {
+  List<String>lines;
+  String title;
+  HadethDisplay({required this.lines,required this.title});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(
+                'assets/images/hadeth_details_bg.png',
+              ),
+              fit: BoxFit.fill)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .03,
+          ),
+          Text(
+            title,
+            style: MyThemeData.lightTheme.textTheme.titleMedium,
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .06,
+          ),
+          lines.isEmpty?
+          Center(child: CircularProgressIndicator()):
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) => Text(lines[index],style:MyThemeData.lightTheme.textTheme.bodyMedium ,textAlign: TextAlign.center,textDirection: TextDirection.rtl,),
+              scrollDirection: Axis.vertical,
+              itemCount: lines.length,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
